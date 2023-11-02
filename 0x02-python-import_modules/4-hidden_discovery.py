bda8cea77de379e607ec9efb8b7b1121a46ccb69
d-nexus <emekaezeobi@outlook.com>
2023-11-02 01:18:54 -0700

    nexus-codes

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if _name_ == "_main_":
+
+    import hidden_4
+
+    # Print sorted name from directory
+
+    for name in sorted(dir(hidden_4)):
+
+        # print only names that do not start with __
+
+        if name[:2] != '__':
+
+            print("{}".format(name))
